1abacaf594a218a44a93c721cd94e9329fda3040
nilesh95 <nayalneel@gmail.com>
2016-07-30 11:14:25 +0530

    Strings files for hindi and arabic added
    string files are added

--- StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/service/StockTaskService.java
+++ StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/service/StockTaskService.java
@@ -186,7 +186,7 @@ public class StockTaskService extends GcmTaskService{
     h.post(new Runnable() {
       @Override
       public void run() {
-        Toast.makeText(mContext, getString(R.string.stock_error), Toast.LENGTH_LONG).show();
+        Toast.makeText(mContext, mContext.getResources().getString(R.string.stock_error), Toast.LENGTH_LONG).show();
       }
     });
   }
